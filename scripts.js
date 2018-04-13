284bc368686b6a71a24116609a6060673d629a8e
Patrick Hansen <pah@pandisign.com>
2018-04-13 10:31:50 +0200

    Added ovveride for children() function

--- scripts.js
+++ scripts.js
@@ -16,3 +16,17 @@ jQuery.fn.parent = function(depth) {
         return $(_context);
     }
 };
+
+/*
+Overrides default children() functionality of jquery if a depth number is specified, if not returns all children just as the normal jquery children function would.
+Can be used totally like the normal jquery children function but can also be used like so:
+children(2) will get the 2 first children of specified parent.
+ */
+jQuery.fn.children = function(depth) {
+    var _context = this.get(0).children;
+    if(typeof depth === "undefined" || depth == 0){
+        return $(_context);
+    }else{
+        return $(_context).slice(0,depth);
+    }
+};
